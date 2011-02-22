918ceb5127804d21f4896c3f9e4cfadc316b92a6
rgaudin <rgaudin@gmail.com>
2011-02-22 11:15:06 +0000

    added database + examples

---
+++ anm/database.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import Table, Column, Integer, String, \
+                       MetaData, ForeignKey, DateTime, Unicode
+
+engine = create_engine('sqlite:///anm.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+metadata = MetaData()
+
+periods_table = Table('period', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', Unicode(50)),
+    Column('start_on', DateTime),
+    Column('end_on', DateTime),
+)
+
+accounts_table = Table('account', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('number', String(8), unique=True),
+    Column('name', Unicode(100)),
+)
+
+budgets_table = Table('budget', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('account_id', Integer, ForeignKey('account.id')),
+    Column('period_id', Integer, ForeignKey('period.id')),
+    Column('amount', Integer),
+)
+
+operations_table = Table('operation', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('account_id', Integer, ForeignKey('account.id')),
+    Column('order_number', String(20), unique=True),
+    Column('invoice_number', String(20)),
+    Column('invoice_date', DateTime),
+    Column('provider', Unicode(100)),
+    Column('amount', Integer),
+)
+
+metadata.create_all(engine)
+
+class Period(object):
+    def __init__(self, name, start_on, end_on):
+        self.name = name
+        self.start_on = start_on
+        self.end_on = end_on
+
+    def __repr__(self):
+        return "<Period('%s', '%s')>" % (self.start_on, self.end_on)
+
+    def __unicode__(self):
+        return self.name
+
+class Account(object):
+    def __init__(self, number, name):
+        self.number = number
+        self.name = name
+
+    def __repr__(self):
+        return "<Account('%s')>" % (self.number)
+
+class Budget(object):
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __repr__(self):
+        return "<Budget('%s','%s')>" % (self.period, self.amount)
+
+class Operation(object):
+    def __init__(self, order_number, invoice_number, invoice_date, provider, amount):
+        self.order_number = order_number
+        self.invoice_number = invoice_number
+        self.invoice_date = invoice_date
+        self.provider = provider
+        self.amount = amount
+
+    def __repr__(self):
+        return "<Operation('%s','%s')>" % (self.account, self.amount)
+
+mapper(Account, accounts_table, properties={
+    'operations': relationship(Operation, backref='account'),
+    'budgets': relationship(Budget, backref='account')
+})
+mapper(Period, periods_table, properties={
+    'budgets': relationship(Budget, backref='period')
+})
+mapper(Budget, budgets_table)
+mapper(Operation, operations_table)
---
+++ anm/test_database.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+from datetime import datetime
+
+from database import *
+
+q1 = Period(u'Q1 2011', start_on=datetime(2011, 01, 01), end_on=datetime(2011, 03, 31))
+
+perso = Account('2344d', u'Personnel')
+batim = Account('23ds3', u'Bâtiment')
+
+bp = Budget(205765890)
+bp.account = perso
+bp.period = q1
+
+bb = Budget(12560000)
+bb.account = batim
+bb.period = q1
+
+achat = Operation('12', '201189n', datetime.today(), u'yɛlɛman s.à.r.l', 950000)
+achat.account = perso
+
+session.add_all((perso, batim, bp, bb, q1, achat))
+session.commit()
+
+all_accounts = session.query(Account).all()
+
+
+
+
+    
+
