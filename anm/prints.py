6544c0504f55215086334c0add91fd6a104f7ffd
Alou Dolo <aloud7@gmail.com>
2011-03-09 10:39:47 +0000

    pdf: list of operations

--- anm/prints.py
+++ anm/prints.py
@@ -51,39 +51,16 @@ def build_accounts_report(period, filename=None, format='pdf'):
 
 
 def build_operations_report(account, period, filename=None, format='pdf'):
+    """ PDF: List of operations"""
+    doc = Document(title=_(u"The list of operations for the period %s.") \
+                    % period, landscape=False, stick_sections=True)
 
-    doc = Document(title=_(u"List operations for %s") % period, \
-                   landscape=False)
-
-    table = Table(5)
-    table.add_header_row([
-            Text(_(u"Order number")),
-            Text(_(u"Invoice number")),
-            Text(_(u"Invoice date")),
-            Text(_(u"Provider")),
-            Text(_(u"Amount"))])
-
-    # column widths
-    table.set_column_width(10, 0)
-    table.set_column_width(10, 1)
-    table.set_column_width(15, 2)
-    table.set_column_width(15, 4)
-
-    # column alignments
-    table.set_alignment(Table.ALIGN_LEFT, column=0)
-    table.set_alignment(Table.ALIGN_LEFT, column=1)
-    table.set_alignment(Table.ALIGN_LEFT, column=2)
-    table.set_alignment(Table.ALIGN_LEFT, column=3)
-    table.set_alignment(Table.ALIGN_LEFT, column=4)
     if account == None:
-        operations = [(operation.order_number, operation.invoice_number,\
-                      operation.invoice_date.strftime('%F'),\
-                      operation.provider, operation.amount, operation) \
-                      for operation in session.query(Operation).\
-                      group_by(Operation.account).\
-                      order_by(desc(Operation.invoice_date)).all()]
-
+        accounts = session.query(Account).all()
     else:
+        accounts = session.query(Account).filter_by(number=account.number).all()
+
+    for account in accounts:
         operations = [(operation.order_number, operation.invoice_number,\
                       operation.invoice_date.strftime('%F'),\
                       operation.provider, operation.amount, operation) \
@@ -91,16 +68,43 @@ def build_operations_report(account, period, filename=None, format='pdf'):
                       filter_by(account=account, period=period).\
                       order_by(desc(Operation.invoice_date)).all()]
 
-    if operations:
-        for operation in operations:
-            table.add_row([
-                Text(operation[0]),
-                Text(operation[1]),
-                Text(operation[2]),
-                Text(operation[3]),
-                Text(operation[4])])
+        if operations:
 
-        doc.add_element(table)
+            section_name = (_('%(name)s %(number)s'))\
+                               % {'name': account.name,\
+                                  'number': account.number}
+            doc.add_element(Section(section_name))
+            doc.add_element(Paragraph(_(u'')))
+            table = Table(5)
+            table.add_header_row([
+                    Text(_(u"Order number")),
+                    Text(_(u"Invoice number")),
+                    Text(_(u"Invoice date")),
+                    Text(_(u"Provider")),
+                    Text(_(u"Amount"))])
+
+            # column widths
+            table.set_column_width(10, 0)
+            table.set_column_width(10, 1)
+            table.set_column_width(15, 2)
+            table.set_column_width(15, 4)
+
+            # column alignments
+            table.set_alignment(Table.ALIGN_LEFT, column=0)
+            table.set_alignment(Table.ALIGN_LEFT, column=1)
+            table.set_alignment(Table.ALIGN_LEFT, column=2)
+            table.set_alignment(Table.ALIGN_LEFT, column=3)
+            table.set_alignment(Table.ALIGN_LEFT, column=4)
+
+            for operation in operations:
+                table.add_row([
+                    Text(operation[0]),
+                    Text(operation[1]),
+                    Text(operation[2]),
+                    Text(operation[3]),
+                    Text(operation[4])])
+
+            doc.add_element(table)
 
     gen = PDFGenerator(doc, filename)
     gen.render_document()
