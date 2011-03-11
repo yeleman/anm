b51d49a14e9b6dd3f6211718ffb1759ca28a4a7d
rgaudin <rgaudin@gmail.com>
2011-03-11 09:38:29 +0000

    gettext windows

--- anm/anm.py
+++ anm/anm.py
@@ -3,16 +3,23 @@
 # maintainer: rgaudin
 
 import sys
-
 import gettext
+import locale
+
 from PyQt4 import QtGui
 
 import fixture_data
+import gettext_windows
 from ui import MainWindow
 from ui.window import ANMWindow
 
 
 def main():
+
+    gettext_windows.setup_env()
+
+    locale.setlocale(locale.LC_ALL, '')
+
     gettext.install('anm', localedir='locale', unicode=True)
 
     # ensure DB is in place
